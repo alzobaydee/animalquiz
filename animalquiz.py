410c40bb578b6195e4a57ba45d446cab5928d952
AlZoSai <saifalzobaydee@hotmail.com>
2018-11-18 19:52:06 +0100

    more points to the people :)

--- animalquiz.py
+++ animalquiz.py
@@ -5,20 +5,20 @@ def guessing_controller(guess, answer):
     tries = 0
 
     while is_still_guessing and tries < 3:
-        if guess == answer:
+        if guess.lower() == answer.lower():
             print('Right on!')
-            points = points + 1
+            points = points + 3 - tries
             is_still_guessing = False
         else:
             if tries < 2:
-                guess = input('Nope, try again ')        
+                guess = input('Nope, try again ')
             tries = tries + 1
     if tries == 3:
         print("Whaaat? you couldn't get this one? it's: " + answer)
         
 points = 0
 print('Guess the animal')
-answer1 = input('What bear lives in the north pool?')
+answer1 = input('What bear lives in the north pole?')
 guessing_controller(answer1, 'ice bear')
 
 answer2 = input('what animal is the fastest?')
@@ -27,4 +27,9 @@ guessing_controller(answer2, 'cheetah')
 answer3 = input('which animal is the BIGGEST?')
 guessing_controller(answer3, 'blue whale')
 
-print('yo points aa: ' + str(points))
+answer4 = input('which of these choices is a fish? \n \
+A) Whale\n B) Something\n C) Shark\n \
+Write A, B or C ')
+guessing_controller(answer4, 'c')
+
+print('yo points aa: ' + str(points) + ' of total: 12')
